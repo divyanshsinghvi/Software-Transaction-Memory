af332f4729d448cbb582762faa52f43a3214fd32
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-10-27 01:04:17 +0530

    cleanup

--- src/Ref.java
+++ src/Ref.java
@@ -1,8 +1,2 @@
-
-public final class Ref<T>{
-        RefVersion<T, Long> snaps;
-
-        public Ref(T value){
-                snaps = RefVersion.get(value, 0L);
-        }
+public class Ref {
 }
---
+++ src/STM.java
@@ -0,0 +1,36 @@
+public class STM {
+
+    //Operations
+
+    //Optimistic implementation of STM read
+    public void read()
+    {
+        // Direct read of memory location
+
+
+        //Validate read data
+
+
+        //Insert in read set
+
+
+        // Return value
+
+    }
+
+    public void write()
+    {
+
+    }
+
+    public void readsValid()
+    {
+
+    }
+
+    public  void commit()
+    {
+
+    }
+
+}
