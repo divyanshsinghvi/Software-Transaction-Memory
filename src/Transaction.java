7f57b86f296da6b55ad3fd9c4ea67f89ee51f295
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-11-03 01:21:32 +0530

    Deleted previous attempts- let's start afresh

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,12 +1,2 @@
 public class Transaction {
-    TxnDescriptor descriptor;
-    TxnMemento memento;
-
-    public Transaction(TxnDescriptor descriptor, TxnMemento memento) {
-        this.descriptor = descriptor;
-        this.memento = memento;
-    }
-
-   
-
 }
--- src/TxnMemento.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashMap;
-
-public class TxnMemento {
-    private HashMap<Ref, Long> version = new HashMap<>();
-}
-
-
-
