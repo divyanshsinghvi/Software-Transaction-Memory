0ab3bbd5e1c0fe55829862067cece6120ebce87f
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-11-03 15:17:46 +0530

    Little bit of lock implemented

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Classes and their functionalitites -
 -- Atomic
 -- Transaction
 
+ToDo list
+
+
+Things to be taken care for:
+a) Clock locking system :: CHeck if it's working
+b)
+
--- src/Clock.java
+++ src/Clock.java
@@ -1,17 +1,33 @@
 import java.math.BigInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Clock {
     int count;
+    ReadWriteLock lock = new ReentrantReadWriteLock();
+    Lock writeLock = lock.writeLock();
+    Lock readLock = lock.readLock();
     Clock(){
         count = 0;
     }
 
     public int getGlobalCount(){
-        return count;
+        try {
+            readLock.lock();
+            return count;
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void incGlobalCount(){
-        count++;
+        try {
+            writeLock.lock();
+            count++;
+        } finally {
+            writeLock.unlock();
+        }
     }
 
 
