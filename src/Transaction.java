4bc308e2be3627819243d21e230c8597e5489ccf
meghaagr13 <agarwalmegha1308@gmail.com>
2018-11-03 16:41:47 +0530

    restructured code

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,44 +1,47 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Transaction {
+    int transactionId;
     int readStamp,writeStamp;
-    HashSet<> readSet, writeSet;
+    HashMap<TVar,TVar> readMap, writeMap;
 
     Transaction(){
-        readSet = new HashSet();
-        writeSet = new HashSet();
+        readMap = new HashMap();
+        writeMap = new HashMap();
         readStamp =  getGlobalCount();
+        transactionId = getId();
     }
 
-    public void read(TVar x){
-        if(x in writeSet){
-            return writeSet[x];
+    public TVar read(TVar x){
+        if(writeMap.get(x)!=null){ // If local copy exist
+            return writeMap.get(x);
         }
-        else if(x.lock is locked || x.stamp > readStamp ){
+        else if(x.lock == 1 || x.stamp > readStamp ){ //If local copy does not exist check validity
             Abort();
         }
         else{
-            readSet[x] = x.version;//version check
+            TVar z = new TVar(x);
+            readMap.put(x,z);
         }
     }
 
-    public void write(TVar x){
-        if (x in writeSet){
-            writeSet[x] = x.version;
+    public void write(TVar x,TVar y){
+        if (writeMap.get(x)!=null){
+            writeMap.put(x,y);
         }
         else{
-            writeSet[x] = x.version;
+            writeMap.put(x,y);
         }
     }
 
     public void commit(){
-        for(i in writeSet)
-        {
-            i.lock;
+        for(Map.Entry<TVar,TVar> entry:writeMap) {
+            entry.getKey().lock = 1;
         }
         incGlobalClock();
         writeStamp = getGlobalCount();
-        for(i in readSet)
+        for()
         {
             if(map[i].lock != currentTransaction || map[i].stamp > readStamp)
             {
