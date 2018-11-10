e2708ffe96a526860ced6fb0817f8689be16ae73
Megha Agarwal <agarwalmegha1308@gmail.com>
2018-11-10 16:13:38 +0530

    Trying composability

--- src/Transaction.java
+++ src/Transaction.java
@@ -31,16 +31,15 @@ public class Transaction {
                 if(v1!=v2 || v1>readStamp || (x.lock.intValue()!=0 && x.lock.intValue()!=transactionId)){
                     return Abort();
                 }
-
                 TVar z = new TVar(x);
                 readMap.put(x,z);
-              v2 = x.stamp.intValue();
-              if(v1!=v2)
-              {
+                v2 = x.stamp.intValue();
+                if(v1!=v2)
+                {
                   readMap.remove(x,z);
                   return Abort();
-              }
-              return result;
+                }
+                return result;
           }
 
 //          else if (x.lock.intValue() != 0 || x.stamp.intValue() > readStamp) { //If local copy does not exist check validity
--- test/Composability.java
+++ test/Composability.java
@@ -1,2 +1,73 @@
+import com.sun.org.apache.xml.internal.security.algorithms.implementations.IntegrityHmac;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.*;
+
+
+class MyQueue extends Thread {
+    TVar<Queue<Integer>> q1;
+    TVar<Queue<Integer>> q2;
+    public MyQueue (TVar<Queue<Integer>> a,TVar<Queue<Integer>> b) {
+        this.q1 = a;
+        this.q2 = b;
+        //System.out.println(a.value);
+    }
+
+    public void printQueue(){
+        int x = q1.value.size() + q2.value.size();
+        System.out.println(x);
+    }
+
+    public void run() {
+        boolean condition=false;
+        do {
+            Transaction tx = new Transaction();
+                Object d = tx.read(q1);
+                if (d instanceof Integer)
+                    continue;
+            Queue<Integer> temp = (Queue<Integer>)d;// tx.read(q1);
+            int head;
+            try {
+                head = temp.remove();
+            }catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+            TVar<Queue<Integer>> tempQueue = new TVar<>(q2);
+
+            tempQueue.value.add(head);
+            tx.write(q2,tempQueue);
+            condition = tx.commit();
+        }while (!condition);
+        printQueue();
+        //printQueue(this.q2.value);
+
+    }
+}
+
 public class Composability {
+    public static void main(String[] args){
+        TVar<Queue<Integer>> queue1 = new TVar<>();
+        TVar<Queue<Integer>> queue2 = new TVar<>();
+        queue1.value = new LinkedList<>();
+        queue2.value = new LinkedList<>();
+        MyQueue[] threads = new MyQueue[200];
+        queue1.value.add(3);
+        queue2.value.add(4);
+        for(int i=0;i<2;i++){
+            threads[i] = new MyQueue(queue1,queue2);
+        }
+
+        for(int i=0;i<2;i++){
+            threads[i].start();
+        }
+
+
+    }
+
+
+
 }
