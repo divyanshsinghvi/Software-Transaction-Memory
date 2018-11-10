02e2566785aa5fa00f6ff4028fe09c4f69da76bc
meghaagr13 <agarwalmegha1308@gmail.com>
2018-11-10 21:49:42 +0530

    hash test

--- test/HashMapTest.java
+++ test/HashMapTest.java
@@ -23,12 +23,34 @@ class HashThread extends Thread {
 
 }
 
+class HashThread2 extends Thread {
+    HashMap<Integer> a;
+    int value;
+
+    public HashThread2 (HashMap<Integer> a,int value) {
+        this.a = a;
+        this.value = value;
+        // a.printMap();
+    }
+
+    public void run() {
+        boolean condition = false;
+        do {
+            Transaction tx = new Transaction();
+            if(!a.putItem("AA"+value,value,tx))
+                continue;
+            condition = tx.commit();
+        }while(!condition);
+      //  a.printMap();
+    }
+
+}
+
+
 
 public class HashMapTest {
 
-    public static void main(String args[]) throws InterruptedException {
-        HashMap<Integer> hashMap = new HashMap<>();
-
+    public static void test1(HashMap<Integer> hashMap){
         Transaction tx = new Transaction();
         boolean condition = false;
         do {
@@ -36,8 +58,8 @@ public class HashMapTest {
                 continue;
             condition  = tx.commit();
         }while(!condition);
-        hashMap.printMap();
-        System.out.println("--------------");
+        //hashMap.printMap();
+        //System.out.println("--------------");
         //Map Initialized
         int n =500;
         HashThread[] threads = new HashThread[n];
@@ -49,6 +71,38 @@ public class HashMapTest {
         System.out.println("I am here");
         for(int i=0;i<n;i++)
             threads[i].start();
+
+    }
+
+    public static void test2(HashMap<Integer> hashMap) throws InterruptedException {
+        Transaction tx = new Transaction();
+        boolean condition = false;
+        do {
+            if(!hashMap.putItem("AA", 3, tx))
+                continue;
+            condition  = tx.commit();
+        }while(!condition);
+        //hashMap.printMap();
+        //System.out.println("--------------");
+        //Map Initialized
+        int n =200;
+        HashThread2[] threads = new HashThread2[n];
+        for(int i=0;i<n;i++){
+            threads[i] = new HashThread2(hashMap,i);
+//           threads[i].sleep(100);
+        }
+
+        System.out.println("I am here");
+        for(int i=0;i<n;i++)
+            threads[i].start();
+
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        HashMap<Integer> hashMap = new HashMap<>();
+        test2(hashMap);
+        Thread.sleep(300);
+        hashMap.printMap();
 
     }
 
